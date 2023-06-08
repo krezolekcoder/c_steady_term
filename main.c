a7ceebef9ea9011961b83a07fef3a2f219a5d4ec
krezolekcoder <kkrezole@gmail.com>
2023-06-08 16:48:27 +0200

    changed algorithm for line writing

--- main.c
+++ main.c
@@ -8,36 +8,6 @@
 #define FRAME_WIDTH  128
 #define FRAME_HEIGHT 64
 
-static char screen[FRAME_WIDTH * FRAME_HEIGHT];
-
-uint32_t write_text_to_frame_buffer(char *frame_buffer, int x, int y, const char *format, ...)
-{
-    va_list args;
-    va_start(args, format);
-
-    // Use vsprintf to format the string and write it to the frame buffer
-    int written = vsprintf(&frame_buffer[y * FRAME_WIDTH + x], format, args);
-
-    va_end(args);
-
-    return written;
-}
-
-
-void draw_line(char *frame_buffer, char ch, uint32_t x_start, uint32_t y_start, uint32_t x_end, uint32_t y_end)
-{
-    // uint32_t x = 0;
-    // for (int y = 0; y < 5; y++) {
-    //     screen[y * FRAME_WIDTH + x] = '*';
-    // }
-
-    // calculate dy/dx
-    // uint32_t dy    = labs(y_end - y_start);
-    // uint32_t dx    = labs(x_end - x_start);
-    // uint32_t dy_dx = dy / dx;
-}
-
-
 
 void set_console_size(int width, int height)
 {
@@ -62,6 +32,52 @@ void write_console_output(const char *char_buffer, int width, int height)
     WriteConsoleOutputCharacterA(std_handle, char_buffer, width * height, start_coord, &chars_written);
 }
 
+uint32_t write_text_to_frame_buffer(char *frame_buffer, int x, int y, const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    // Use vsprintf to format the string and write it to the frame buffer
+    int written = vsprintf(&frame_buffer[y * FRAME_WIDTH + x], format, args);
+
+    va_end(args);
+
+    return written;
+}
+
+
+void draw_line(char *frame_buffer, char ch, uint32_t x_start, uint32_t x_end, uint32_t y_start, uint32_t y_end)
+{
+
+    // calculate dy/dx
+
+    uint32_t dy = labs(y_end - y_start);
+    uint32_t dx = labs(x_end - x_start);
+
+    uint32_t derivative = 0;
+
+    if (dy > dx) {
+        derivative = dy / dx;
+
+        for (int x = x_start; x < x_end; x++) {
+            for (int y = x * derivative; y < x * derivative + derivative; y++) {
+                frame_buffer[y * FRAME_WIDTH + x] = ch;
+            }
+        }
+    }
+    else {
+        derivative = dx / dy;
+
+        for (int y = y_start; y < y_end; y++) {
+            for (int x = y * derivative; x < y * derivative + derivative; x++) {
+                frame_buffer[y * FRAME_WIDTH + x] = ch;
+            }
+        }
+    }
+}
+
+
+
 int main()
 {
 
@@ -75,13 +91,9 @@ int main()
 
     // Display the character framebuffer
 
-    for (int y = 0; y < 20; y++) {
-        framebuffer[y * FRAME_WIDTH + 0] = '*';
-    }
+    draw_line(framebuffer, '*', 0, 0, 0, 10);
 
     write_console_output(framebuffer, FRAME_WIDTH, FRAME_HEIGHT);
-
-
 
     getch();
     return 0;
